fd70b74bcb749a2b2ca06aeff48426388458a106
Erik Vullings <erik.vullings@gmail.com>
2020-01-13 00:08:32 +0100

    Changing name of db

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -4,7 +4,7 @@ import { createApi, db } from 'rest-easy-loki';
 export const collectionName = 'documents';
 
 const port = process.env.LOKI_PORT || '3030';
-const dbName = process.env.LOKI_DB || './db/locatieregister.db';
+const dbName = process.env.LOKI_DB || './db/dilemmatrainer.db';
 const cors = (process.env.LOKI_CORS || 'true') === 'true';
 const sizeLimit = process.env.LOKI_SIZE_LIMIT || '250mb';
 
